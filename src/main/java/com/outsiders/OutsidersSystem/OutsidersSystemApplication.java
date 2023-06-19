2e3348a399d5680f35c02c72c5c86520c9656b15
Guilherme Wanderley Espinola <gwanderleyespinola@gmail.com>
2023-06-19 20:28:38 -0300

    commit inicial

---
+++ src/main/java/com/outsiders/OutsidersSystem/OutsidersSystemApplication.java
@@ -0,0 +1,13 @@
+package com.outsiders.OutsidersSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OutsidersSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OutsidersSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/outsiders/OutsidersSystem/OutsidersSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.outsiders.OutsidersSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OutsidersSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
